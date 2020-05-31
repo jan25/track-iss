c273f8a40050b1adbd6e67730969be4e0504044e
Abhilash Gnan <abhilashgnan@gmail.com>
2020-05-31 22:25:46 +0200

    Cleanup

--- src/scripts/cities_json.js
+++ src/scripts/cities_json.js
@@ -1,49 +1,19 @@
-const csv = require('csv-parser')
-const fs = require('fs')
-const worldCities = require('./worldcities.json');
-const distance = require('@turf/turf').distance;
+const csv = require('csv-parser');
+const fs = require('fs');
 const results = [];
- 
-let getNearestCity = (latLong)=> {
-  let near;
-  let minDist = 10000; // 'km'
-  for (let city of worldCities) {
-      let d = distance(
-          [parseFloat(city.lng), parseFloat(city.lat)],
-          [latLong.longitude, latLong.latitude],
-          {
-              units: 'kilometers'
-          }
-      );
-      if (d < minDist) {
-          minDist = d;
-          near = city;
-      }
-  }
-  return {
-      city: near.city,
-      country: near.country,
-      // status: Status.Available
-  };
-}
 
-console.log(getNearestCity({
-  latitude: Math.random() * 100,
-  longitude: Math.random() * 100
-}))
-
-// fs.createReadStream('worldcities.csv')
-//   .pipe(csv())
-//   .on('data', (data) => 
-//     results.push({
-//       city: data.city,
-//       country: data.country,
-//       lat: data.lat,
-//       lng: data.lng
-//     })
-//   )
-//   .on('end', () => {
-//     console.log(results);
-//     let jsonStr = JSON.stringify(results);
-//     fs.writeFileSync("worldcities.json", jsonStr);
-//   });
+fs.createReadStream('worldcities.csv')
+  .pipe(csv())
+  .on('data', (data) => 
+    results.push({
+      city: data.city,
+      country: data.country,
+      lat: data.lat,
+      lng: data.lng
+    })
+  )
+  .on('end', () => {
+    console.log(results);
+    let jsonStr = JSON.stringify(results);
+    fs.writeFileSync("worldcities.json", jsonStr);
+  });
